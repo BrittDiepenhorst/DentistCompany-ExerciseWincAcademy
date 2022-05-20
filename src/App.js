ad27081074d03a514a16c17e417a31eda4170e07
Britt Diepenhorst <brittdiepenhorst@hotmail.com>
2022-05-20 10:19:43 +0200

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import "./Day.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
 import Home from "./Home";
 import Calendar from "./Calendar";
 import Day from "./Day";
-import Changes from "./Changes";
-
+import Form from "./Form";
 import generateRandomAppointments from "./utils/utils"
+
 const appointments = generateRandomAppointments(150);
 
+// const patients = appointments.patient;
+const dentists = appointments.map(appointment => appointment.dentist);
+console.log(dentists);
+
 function App() {
+  const [dentist, setDentist] = useState({});
+  // let [sickPeople, setSickPeople] = useState({ dentist: [], assistant: [], patient: [] })
+  // const [patient, setPatients] = useState({ patients });
+  // const [employees, setEmployee] = useState({ appointments });
+
+  const makeSick = (employee) => {
+    console.log('You clicked submit.');
+    console.log(employee);
+    console.log(employee.dentist.id);
+
+    const sickEmployee = dentists.find(dentist => dentist.id === employee.dentist.id);
+    sickEmployee.sick = true;
+
+    console.log(sickEmployee);
+
+    setDentist({
+      ...dentist,
+      sickEmployee
+    });
+
+    const makeRed = () => {
+      const element = document.getElementsByClassName('dentist');
+      console.log(element);
+      // element.classList.toggle('red');
+    };
+
+    if (sickEmployee.sick === true) {
+      makeRed(sickEmployee);
+    };
+
+    console.log(employee);
+
+    // setSickPeople({
+    //   ...sickPeople,
+    //   [sickPeople.dentist]: employee,
+    // });
+    // console.log(sickPeople.dentist);
+  };
+
+  // const addPatient = (firstName, lastName, phoneNumber, emailAddress) => {
+  //   setPatients([
+  //     ...patients,
+  //     {
+  //       id: patients.length + 1,
+  //       firstName,
+  //       lastName,
+  //       phoneNumber,
+  //       emailAddress
+  //     }
+  //   ])
+  // }
+
   return (
     < Router >
       <div>
@@ -26,7 +82,7 @@ function App() {
               <Link to="/day">Day view</Link>
             </li>
             <li>
-              <Link to="/changes">Change Calendar</Link>
+              <Link to="/form">Form to change the calendar</Link>
             </li>
           </ul>
         </nav>
@@ -39,8 +95,8 @@ function App() {
             <Route path="/day">
               <Day appointments={appointments.filter(app => app.day === 1)} />
             </Route>
-            <Route path="/changes">
-              <Changes appointments={appointments} />
+            <Route path="/form">
+              <Form appointments={appointments} /* addPatient={addPatient} */ makeSick={makeSick} />
             </Route>
             <Route path="/">
               <Home />
